c648e659853034a45e2652b5a455c8972d69a995
Jeremy Gibbons <jeremy.gibbons@laposte.net>
2017-04-16 11:45:57 +0200

    Clean up comments and clarify usage

--- PRTGAddRouters/AddRouters.ps1
+++ PRTGAddRouters/AddRouters.ps1
@@ -1,9 +1,50 @@
-#
+#################################
 # AddRouters.ps1
-#
+# By Jeremy Gibbons, April 2017
 # Inspired by https://thedomainiown.wordpress.com/prtg-related/prtg-script-powershell-admin-module/
-#
+#################################
 
+#######################
+#
+# This script expects to find a CSV file in c:\temp\arg.csv
+# The CSV file should contain a header line with columns
+# - RouterName => US1234-RT01
+# - SiteName => US1234 - New York City
+# - SubGroup => Midwest
+# - IPAddr => 10.5.34.1
+# - IfIndex => 3 (this is the snmp index)
+# - IfAlias => optional, this is the fixed name of the interface in snmp (while the index may change)
+#
+#######################
+
+############################
+# INSTANCE SPECIFIC VARIABLES
+# MUST BE UPDATED PRIOR TO EXECUTING THIS SCRIPT
+############################
+$user = "ADM_jeremy.gibbons"
+$passhash = "1144530515"
+
+# PRTG server, either IP or hostname (do not include http:// or https://)
+$PRTGHost = "172.31.230.79"
+
+# The PRTG id of an empty group for a new site
+$siteTemplateID = "10019";
+# The PRTG id of an empty device for a new router
+$routerTemplateID = "10020";
+# The PRTG id of an existing ping sensor
+$pingSensorTemplateID = "2578";
+# The PRTG id of an existing snmptraffic sensor
+$snmpSensorTemplateID = "2579";
+
+# The PRTG ID of the group into which all new sites should be placed
+$StartingID=10023
+
+#############################
+# END INSTANCE SPECIFIC VARIABLES
+#############################
+
+
+# This part is necessary to avoid certificate validation errors when connecting to https servers
 add-type @"
     using System.Net;
     using System.Security.Cryptography.X509Certificates;
@@ -17,27 +58,8 @@ add-type @"
 "@
 [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
 
-############################
-# INSTANCE SPECIFIC VARIABLES
-# MUST BE UPDATED PRIOR TO EXECUTING THIS SCRIPT
-############################
-$auth = "username=ADM_jeremy.gibbons&passhash=1144530515"
-$PRTGHost = "172.31.230.79"
-
-$siteTemplateID = "10019";
-$routerTemplateID = "10020";
-$pingSensorTemplateID = "2578";
-$snmpSensorTemplateID = "2579";
-
-#Parent group id for Airgas sites
-$StartingID=10023
-
-#############################
-# END INSTANCE SPECIFIC VARIABLES
-#############################
-
-#Find the group id for a given airgas region
-($argGroups | Where {$_.name -eq "East"}).objid
+# The auth string used in the REST calls
+$auth = "username=$user&passhash=$passhash"
 
 $csvlines = import-csv c:\temp\arg.csv;
 foreach($line in $csvlines)
@@ -49,17 +71,17 @@ foreach($line in $csvlines)
 	$ifIndex=$line.IfIndex;
 	$ifAlias=$line.IfAlias;
 
-	#Retrieve list of groups in the airgas group
+	# Retrieve list of groups in the airgas group
 	$url = "https://$PRTGHost/api/table.xml?content=groups&output=xml&columns=objid,group,name,parentid&count=2500&id=$StartingID&$auth"
 	$restresp = Invoke-RestMethod -Uri $url -MaximumRedirection 0 -ErrorAction Ignore
 	$argGroups = $restresp.groups.item
 
-	#Retrieve list of devices in the airgas group
+	# Retrieve list of devices in the airgas group
 	$url = "https://$PRTGHost/api/table.xml?content=devices&output=xml&columns=objid,group,device,host&count=2500&id=$StartingID&$auth"
 	$restresp = Invoke-RestMethod -Uri $url -MaximumRedirection 0 -ErrorAction Ignore
 	$argDevices = $restresp.devices.item
 
-	#Find the Group ID for the given Airgas region
+	# Find the Group ID for the given Airgas region
 	$subGroupId = ($argGroups | Where {$_.name -eq $subgroup}).objid;
 
 	# Check if site group already exists.
@@ -78,7 +100,7 @@ foreach($line in $csvlines)
 		$siteID = ($argGroups | Where {$_.name -eq $siteName}).objid;
 	}	
 
-	#Check if device already exists.
+	# Check if device already exists.
 	$deviceID = "0";
 	if(($argDevices | Where {$_.device -eq $routerName}) -eq $null)
 	{
@@ -88,7 +110,7 @@ foreach($line in $csvlines)
 		$request = Invoke-WebRequest -Uri $url -MaximumRedirection 0 -ErrorAction Ignore;
 		$deviceID = $request.Headers.Location.Split("=")[1]
 
-		#Set the router's IP
+		# Set the router's IP
 		$url = "https://$PRTGHost/api/setobjectproperty.htm?id=$deviceID&name=host&value=$routerIP&$auth" 
 		$request = Invoke-WebRequest -Uri $url -MaximumRedirection 0 -ErrorAction Ignore
 
@@ -120,19 +142,19 @@ foreach($line in $csvlines)
 		$url = "https://$PRTGHost/api/setobjectproperty.htm?id=$snmpID&name=interfacenumber&value=$interfacenumber&$auth" 
         $request = Invoke-WebRequest -Uri $url -MaximumRedirection 0 -ErrorAction Ignore
 
-		#Resume the group (it is created paused)
+		# Resume the group (it is created paused)
 		$url = "https://$PRTGHost/api/pause.htm?id=$siteID&action=1&$auth"
         $request = Invoke-WebRequest -Uri $url -MaximumRedirection 0 -ErrorAction Ignore
 		
-		#Resume the device (it is created paused)
+		# Resume the device (it is created paused)
 		$url = "https://$PRTGHost/api/pause.htm?id=$deviceID&action=1&$auth"
         $request = Invoke-WebRequest -Uri $url -MaximumRedirection 0 -ErrorAction Ignore
 
-		#Resume the ping sensor (it is created paused)
+		# Resume the ping sensor (it is created paused)
 		$url = "https://$PRTGHost/api/pause.htm?id=$pingID&action=1&$auth"
         $request = Invoke-WebRequest -Uri $url -MaximumRedirection 0 -ErrorAction Ignore
 
-		#Resume the snmp sensor (it is created paused)
+		# Resume the snmp sensor (it is created paused)
 		$url = "https://$PRTGHost/api/pause.htm?id=$snmpID&action=1&$auth"
         $request = Invoke-WebRequest -Uri $url -MaximumRedirection 0 -ErrorAction Ignore
 	}
